bd58f31224d63a0788826e84cd6e6f31e1fa170b
AkmElias <akmelias11@gmail.com>
2020-04-10 21:03:33 +0600

    first section done

--- react-e-commerce/src/components/counter.jsx
+++ react-e-commerce/src/components/counter.jsx
@@ -5,11 +5,22 @@ class Counter extends Component {
     count: 0,
   };
 
+  handleIncrement = () => {
+    this.setState({ count: (this.state.count += 1) });
+  };
+
   render() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCounter()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <span className={classes}>{this.formatCounter()}</span>
+        <button
+          className="btn btn-secondary btn-sm"
+          onClick={this.handleIncrement}
+        >
+          Increment
+        </button>
       </div>
     );
   }
