c6b6894a29fd45acfc3394a7b7997f68c23bb4e2
AkmElias <akmelias11@gmail.com>
2020-04-10 22:37:58 +0600

    navbar added

--- react-e-commerce/src/index.js
+++ react-e-commerce/src/index.js
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Counters from "./components/counters";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
